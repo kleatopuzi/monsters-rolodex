29570e5a37dc26f03af4184713281b3f42d9f929
kleatopuzi <ktopuzii@gmail.com>
2022-05-12 15:13:26 +0200

    fetch from API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
       // string: "juhuuuuu",
-      monsters: [
-        {
-          name: "Frankenshtaijn",
-          id: "1",
-        },
-        {
-          name: "Drakula ",
-          id: "2",
-        },
-        {
-          name: "Zombie",
-          id: "3",
-        },
-      ],
+      monsters: [],
     };
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
