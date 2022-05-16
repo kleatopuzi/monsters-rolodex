b391274096b81b15ef9822990481720587ddac6a
kleatopuzi <ktopuzii@gmail.com>
2022-05-16 10:53:55 +0200

    test

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
     );
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1>Klea's Personal Monsters</h1>
         <SearchBox
           placeholder="Search Monsters"
           handleChange={this.handleChange}
